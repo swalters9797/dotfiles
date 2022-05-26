4037cddfb3778bd5c02f882abbcabf57d367e494
Scott Walters <swalters9797@gmail.com>
2022-05-26 15:56:21 -0400

    fix hidden

--- nvim/.config/nvim/lua/tele.lua
+++ nvim/.config/nvim/lua/tele.lua
@@ -7,6 +7,7 @@ local builtin = require('telescope.builtin')
 local actions = require('telescope.actions')
 telescope.setup{
 	defaults = {
+        file_ignore_patterns = {".git/"},
 		mappings = {
 			i = {
 				["<C-j>"] = actions.move_selection_next,
@@ -19,8 +20,8 @@ telescope.setup{
 	}
 }
 local opts = { noremap=true }
-vim.api.nvim_set_keymap('n', 'ff', '<cmd>lua require(\'telescope.builtin\').find_files{{hidden=true}, file_ignore_patterns = { \'.git/\' }}<cr>', opts)
-vim.api.nvim_set_keymap('n', 'fg', '<cmd>lua require(\'telescope.builtin\').live_grep{vimgrep_arguments = { \'rg\', \'--color=never\', \'--no-heading\', \'--with-filename\', \'--line-number\', \'--column\', \'--smart-case\', \'-u\', \'--hidden\'}, file_ignore_patterns = { \'.git/\' }}<cr>', opts)
+vim.api.nvim_set_keymap('n', 'ff', '<cmd>lua require(\'telescope.builtin\').find_files({hidden=true})<cr>', opts)
+vim.api.nvim_set_keymap('n', 'fg', '<cmd>lua require(\'telescope.builtin\').live_grep{vimgrep_arguments = { \'rg\', \'--color=never\', \'--no-heading\', \'--with-filename\', \'--line-number\', \'--column\', \'--smart-case\', \'-u\', \'--hidden\'}}<cr>', opts)
 vim.api.nvim_set_keymap('n', 'fgc', '<cmd>lua require(\'telescope.builtin\').current_buffer_fuzzy_find()<cr>', opts)
 vim.api.nvim_set_keymap('n', 'fb', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>', opts)
 vim.api.nvim_set_keymap('n', 'fh', '<cmd>lua require(\'telescope.builtin\').help_tags()<cr>', opts)
