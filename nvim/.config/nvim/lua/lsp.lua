9531cb00ffa7b955725bc08545cdaa1e130959bb
Scott Walters <swalters9797@gmail.com>
2022-10-31 15:33:39 -0400

    update lsp lua

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -76,7 +76,7 @@ cmp.setup({
     }
 })
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
